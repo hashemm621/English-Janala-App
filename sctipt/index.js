53d0a4848284ee3e80a6f8c616d622eb88609f0c
md-hashem <hashemm_621@hotmail.com>
2025-09-04 04:24:09 +0600

    finally complate project

--- sctipt/index.js
+++ sctipt/index.js
@@ -2,6 +2,11 @@ const createElement = (arr) => {
   const htmlElement = arr.map((el) => `<span class="btn">${el}</span>`);
   return htmlElement.join(" ");
 };
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
 const manageSpinner = (status) => {
   if (status === true) {
     document.getElementById("spinner").classList.remove("hidden");
@@ -95,7 +100,9 @@ const displayLevelWord = (words) => {
                 <button onclick="loadWordDetail(${
                   word.id
                 })" class="btn bg-[#1A91FF1A] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="btn bg-[#1A91FF1A] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+                <button onclick="pronounceWord('${
+                  word.word
+                }')" class="btn bg-[#1A91FF1A] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
     `;
