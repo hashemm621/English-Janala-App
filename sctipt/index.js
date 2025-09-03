2fd24fdff9ee9d04916ce870f8e863d23d298b9d
md-hashem <hashemm_621@hotmail.com>
2025-09-04 00:42:36 +0600

    create card section on ui

--- sctipt/index.js
+++ sctipt/index.js
@@ -3,13 +3,31 @@ const loadLessons = () => {
     .then((res) => res.json()) // promise of json data
     .then((json) => displayLessons(json.data));
 };
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+
+  words.forEach((word) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+    
+    `;
+    // wordContainer.appendChild(card);
+  });
+};
 const displayLessons = (lessons) => {
   const levelContainer = document.getElementById("level-container"); // get the container
   levelContainer.innerHTML = ""; // empty the container
   for (let lesson of lessons) {
     const btnDiv = document.createElement("div"); // create the display element
     btnDiv.innerHTML = `
-                        <button class="btn btn-outline btn-primary">
+                        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
                             <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
                         </button>
         `;
